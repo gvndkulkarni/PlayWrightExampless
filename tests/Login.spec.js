28085b6dec069c2d6783ebffc2ac70f2d921bf7f
Govind Ghatnandrekar <Gvndkulkarni5@gmail.com>
2026-05-10 19:06:47 +0530

    added tohavetext assertion in login.spec.js

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -4,8 +4,8 @@ test ('verify login',async ({page})=>{
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     await page.locator("input[name='username']").type("Admin")
-    await expect(page).toHaveValue("Admin")
+    await expect(page).toHaveText("Admin")
     await page.locator("input[name='password']").type("admin123")
-    await expect(page).toHave("Password")
+    await expect(page).toHaveText("Password")
     await page.locator("button[type='submit']").click()
 });
