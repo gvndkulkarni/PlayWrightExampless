4a8d49be7698c18eea2463f07bfb8b2f24b2cd9e
Govind Ghatnandrekar <Gvndkulkarni5@gmail.com>
2026-05-10 19:03:40 +0530

    Assertion added to verify the value of the username field and to check for the presence of the password field on the login page.

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -4,6 +4,8 @@ test ('verify login',async ({page})=>{
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     await page.locator("input[name='username']").type("Admin")
+    await expect(page.locator("input[name='username']")).toHaveValue("Admin")
     await page.locator("input[name='password']").type("admin123")
+    await expect(page).toHave("Password")
     await page.locator("button[type='submit']").click()
 });
